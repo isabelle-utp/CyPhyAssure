25c2897c754295fce6bcba747e72e7cbe1db8432
Simon Foster <simon.foster@york.ac.uk>
2023-05-22 11:34:28 +0100

    Added a script to make a patched version of Isabelle and build all heap images

---
+++ make_IsabelleUTP.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Get the patch for Isabelle
+wget https://raw.githubusercontent.com/isabelle-utp/CyPhyAssure/main/Isabelle2022-CyPhyAssure.diff
+
+# Patch Isabelle
+patch -p1 < Isabelle2022-CyPhyAssure.diff
+
+# Ensure the system heaps option is set, so that heap images are not built to home
+sed -i "s/option system_heaps : bool = false/option system_heaps : bool = true/g
+" etc/options
+
+# Get the CyPhyAssure repository
+cd src
+git clone https://github.com/isabelle-utp/CyPhyAssure.git
+cd CyPhyAssure
+git submodule update --init --recursive
+cd ../..
+echo "src/CyPhyAssure" >> ROOTS
+
+# Build main sessions
+bin/isabelle build -b ITree_VCG
+bin/isabelle build -b Z_Machines
+bin/isabelle build -b Hybrid-Verification
+bin/isabelle build -b Z_Toolkit
+bin/isabelle build -b UTP
+
